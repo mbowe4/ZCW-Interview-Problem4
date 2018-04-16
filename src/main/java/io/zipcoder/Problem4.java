41a25c25c21501a58cbc198ec163838185c03616
mbowe4 <mlbowe@gmail.com>
2018-04-15 23:06:47 -0400

    complete

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,50 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+    private Map<String, Integer> letterMapping = new HashMap<String, Integer>();
+
+    public String checkForPalindromes(String input) {
+        if(isPalindromic(input)) {
+            return "YES";
+        }
+        else {
+            return "NO";
+        }
+    }
+
+    private boolean isPalindromic(String input) {
+        letterMapping = fillMapWithLetterCount(input);
+        int countOfOddNumbers = 0;
+        for (Integer numberOfOccurrences: letterMapping.values()) {
+            if (numberOfOccurrences % 2 != 0) {
+                countOfOddNumbers++;
+            }
+        }
+        return input.length() > 1 && (!lengthIsEven(input) || countOfOddNumbers <= 0) && (lengthIsEven(input) || countOfOddNumbers <= 1);
+    }
+
+    public Map<String, Integer> fillMapWithLetterCount(String input) {
+        char[] characters = input.toCharArray();
+        for(char character : characters) {
+            letterMapping.put(String.valueOf(character), getLetterCount(character, input));
+        }
+        return letterMapping;
+    }
+
+    private boolean lengthIsEven(String input) {
+        return input.length() % 2 == 0;
+    }
+
+    public int getLetterCount(Character letter, String stringToCheck) {
+        int occurrences = 0;
+        for(int i = 0; i < stringToCheck.length(); i++) {
+            if (stringToCheck.charAt(i) == letter) {
+                occurrences++;
+            }
+        }
+        return occurrences;
+    }
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,77 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
 public class Problem4Test {
+
+    @Test
+    public void checkForPalindromesTest() {
+        Problem4 problem4 = new Problem4();
+        String input = "aaabbbb";
+
+        String expected = "YES";
+        String actual = problem4.checkForPalindromes(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkForPalindromesTest2() {
+        Problem4 problem4 = new Problem4();
+        String input = "cdefghmnopqrstuvw";
+
+        String expected = "NO";
+        String actual = problem4.checkForPalindromes(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkForPalindromesTest3() {
+        Problem4 problem4 = new Problem4();
+        String input = "cdcdcdcdeeeef";
+
+        String expected = "YES";
+        String actual = problem4.checkForPalindromes(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkForPalindromesEmptyString() {
+        Problem4 problem4 = new Problem4();
+        String input = "";
+
+        String expected = "NO";
+        String actual = problem4.checkForPalindromes(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getNumberOccurencesTest() {
+        Problem4 problem4 = new Problem4();
+        String input = "aaabbbb";
+
+        int expected = 3;
+        int actual = problem4.getLetterCount('a', input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fillMapTest() {
+        Problem4 problem4 = new Problem4();
+        String input = "aaabbbb";
+        Map<String, Integer> testMap = problem4.fillMapWithLetterCount(input);
+
+        Integer expected = 3;
+        Integer actual = testMap.get("a");
+
+        Assert.assertEquals(expected, actual);
+    }
+
 }
